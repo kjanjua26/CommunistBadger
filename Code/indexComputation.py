a9e255a0f36de3d6929e3376b530ad3311650328
Kamran Janjua <kamranejaz98@gmail.com>
2018-12-18 03:49:15 +0500

    Create indexComputation.py

---
+++ Code/indexComputation.py
@@ -0,0 +1,9 @@
+"""
+    CommunistBadger v1.0.0
+    This is the code for index computation of stock analysis.
+    We perform analysis on news, tweets and stock and compute market index.
+    Market Index: lambda1*PT/TT+lambda2*NT/TT+lambda2*CV(stock(n,t))
+"""
+class MarketIndex():
+    def __init__(self):
+        pass
