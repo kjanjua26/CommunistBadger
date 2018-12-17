d3e40262b0a51b64931dd0ecd01ee48832fb4123
Kamran Janjua <kamranejaz98@gmail.com>
2018-12-18 03:54:03 +0500

    Update indexComputation.py

--- Code/indexComputation.py
+++ Code/indexComputation.py
@@ -4,6 +4,22 @@
     We perform analysis on news, tweets and stock and compute market index.
     Market Index: lambda1*PT/TT+lambda2*NT/TT+lambda2*CV(stock(n,t))
 """
+import predict_sentiment_news, predict_sentiment_tweets
+
 class MarketIndex():
-    def __init__(self):
-        pass
+    def __init__(self, article_name):
+        self.article_name = article_name
+
+    def compute_tweet_index(self):
+        predict_sentiment_tweets._get_tweet_coefficient()
+
+    def compute_news_index(self):
+        sentiment = predict_sentiment_news.SentimentNews(self.article_name)
+        sentiment.graph_sentiment()
+        pos_percentage, neg_percentage = sentiment.compute_score()
+        print("Positive Percentage: ", pos_percentage)
+        print("Negative Percentaage: ", neg_percentage)
+
+if __name__ == '__main__':
+    index = MarketIndex("Google")
+    index.compute_news_index()
